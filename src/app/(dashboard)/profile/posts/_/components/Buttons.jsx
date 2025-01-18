f0a62444cd73a4f67994c8101a38b697f94cbab8
danial daneshmand <daniald11111377@gmail.com>
2025-01-18 10:20:59 -0800

    BreadCrumbs component added

--- src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
+++ src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
@@ -1,14 +1,18 @@
-"use client"
+"use client";
 import { HiMiniPlus, HiOutlinePencil, HiOutlineTrash } from "react-icons/hi2";
 import Button from "./Button";
 import Link from "next/link";
 
 export function CreatePost(id) {
   return (
-    <Button classes=" mr-0 lg:mr-auto text-lg rounded-lg bg-blue-600 justify-between text-white flex items-center w-32 text-lg ">
-      <span >ایجاد پست</span>
-      <span><HiMiniPlus /></span>
-    </Button>
+    <Link href="/profile/posts/create">
+      <Button classes=" mr-0 lg:mr-auto text-lg rounded-lg bg-blue-600 justify-between text-white flex items-center w-32 text-lg ">
+        <span>ایجاد پست</span>
+        <span>
+          <HiMiniPlus />
+        </span>
+      </Button>
+    </Link>
   );
 }
 
@@ -20,11 +24,11 @@ export function DeletePost(id) {
   );
 }
 
-export function EditPost(id=1) {
+export function EditPost(id = 1) {
   return (
     <Link href={`/profile/posts/edit`}>
       <Button classes=" text-xl">
-        <HiOutlinePencil/>
+        <HiOutlinePencil />
       </Button>
     </Link>
   );
